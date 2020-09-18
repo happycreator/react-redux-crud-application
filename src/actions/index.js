5fb67ef68cee59e2f551f872e886994a13a7be0c
伊藤由利子 <>
2020-09-19 00:01:15 +0900

    implement action and creator fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-const increment = () => ({
-    type: 'INCREMENT'
+export const increment = () => ({
+    type: INCREMENT
 })
 
-const decrement = () => ({
-    type: 'DECREMENT'
+export const decrement = () => ({
+    type: DECREMENT
 })
